e1a32e9dcd6070afeeeb4c454fa2439369cbc6e8
Seungjin Yi <options3224@gmail.com>
2022-04-10 12:48:49 +0900

    Window_Main: 대여/반납 불가능한 경우의 이벤트 처리, 전역 함수 추가(데이터프레임 불러오기)

--- Window_Main.py
+++ Window_Main.py
@@ -15,7 +15,22 @@ BOOK_INFO_Y = 20
 BTN_WIDTH = 75
 
 DIR_CSV_USER = "csv/user.csv"
+DIR_CSV_BOOK = "csv/book.csv"
 DIR_CSV_RENT = "csv/rent.csv"
+
+# 전역 함수: 데이터프레임 불러오기
+def load_dataframes():
+    # csv 파일에서 데이터프레임 불러오기
+    df_user = pd.read_csv(DIR_CSV_USER, encoding='CP949')
+    df_book = pd.read_csv(DIR_CSV_BOOK, encoding='CP949')
+    df_rent = pd.read_csv(DIR_CSV_RENT, encoding='CP949', index_col=0)
+
+    # 데이터프레임 인덱스 설정
+    df_user = df_user.set_index(df_user['USER_PHONE'])  # 전화번호를 인덱스로 설정
+    df_book = df_book.set_index(df_book['BOOK_ISBN'])   # ISBN을 인덱스로 설정
+    df_rent.index.name = "RENT_SEQ"     # Auto Increment를 인덱스로 하며, 별칭 설정
+
+    return df_user, df_book, df_rent
 
 # ===========================================================================================
 # 클래스: 메인 윈도우
@@ -75,17 +90,15 @@ class Window_Main():
 
     # 멤버 메소드: [대여] 버튼 이벤트
     def event_book_rent(self):
-        # user.csv 불러오기
-        df_user = pd.read_csv(DIR_CSV_USER, encoding='CP949')
-        df_user = df_user.set_index(df_user['USER_PHONE'])
 
-        # rent.csv 불러오기
-        df_rent = pd.read_csv(DIR_CSV_RENT, encoding='CP949', index_col=0)
-        df_rent.index.name = "RENT_SEQ"
+        # 데이터프레임 불러오기
+        df_user, df_book, df_rent = load_dataframes()
 
-        # 대여 도서 ISBN, 대여자 전화번호 가져오기
-        isbn = self.bookinfo.get_isbn()
-        phone = self.userinfo.get_phone()
+        # 대여 도서의 ISBN과 도서명, 대여자의 이름과 전화번호 구하기
+        book_isbn = self.bookinfo.get_isbn()
+        book_title = df_book["BOOK_TITLE"].loc[book_isbn]
+        user_phone = self.userinfo.get_phone()
+        user_name = df_user["USER_NAME"].loc[user_phone]
 
         # 대여일(오늘 날짜) 가져오기
         today = datetime.now()
@@ -94,45 +107,70 @@ class Window_Main():
         # 반납예정일(2주 뒤 날짜) 가져오기
         due = today + timedelta(weeks=2)
         due_format = due.strftime("%Y%m%d")
+        
+        # 해당 도서의 대여 이력 중 '대여 중'에 해당하는 이력만 추출
+        df_rent_rented = df_rent[["BOOK_ISBN"]][df_rent["RENT_RETURN_DATE"] == -1]
+        
+        # 대여가 불가능한 경우의 이벤트 처리
+        if not df_rent_rented.empty:
+            messagebox.showinfo("도서 대여", "이미 대여중인 도서입니다.")
+            return 1
+        elif df_user["USER_REG"].loc[user_phone] == False:
+            messagebox.showinfo("도서 대여", "탈퇴한 회원은 도서를 대여할 수 없습니다.")
+            return 2
 
-        new_rent = { "BOOK_ISBN": isbn,\
-                    "USER_PHONE": phone,\
+        # 대여 이력 추가
+        new_rent = { "BOOK_ISBN": book_isbn,\
+                    "USER_PHONE": user_phone,\
                     "RENT_DATE": today_format,\
                     "RENT_DUE_DATE": due_format,\
                     "RENT_RETURN_DATE": -1 }
         df_rent = df_rent.append(new_rent, ignore_index=True)
 
-        df_user["USER_RENT_CNT"].loc[phone] += 1
+        # 대여자의 대여 카운트 증가
+        df_user["USER_RENT_CNT"].loc[user_phone] += 1
 
+        # 데이터프레임을 csv 파일에 저장
         df_user.to_csv(DIR_CSV_USER, index=False, encoding='CP949')
         df_rent.to_csv(DIR_CSV_RENT, index=True, encoding='CP949')
 
-        messagebox.showinfo("도서 대여", "도서 대여 완료(이벤트 테스트)")
+        msg = "도서 대여 완료\n"
+        msg += f"- {book_title}({book_isbn})\n"
+        msg += f"- 대여자: {user_name}({user_phone})"
+        messagebox.showinfo("도서 대여", msg)
+        return 0
+
 
     # 멤버 메소드: [반납] 버튼 이벤트
     def event_book_return(self):
-        # user.csv 불러오기
-        df_user = pd.read_csv(DIR_CSV_USER, encoding='CP949')
-        df_user = df_user.set_index(df_user['USER_PHONE'])
 
-        # rent.csv 불러오기
-        df_rent = pd.read_csv(DIR_CSV_RENT, encoding='CP949', index_col=0)
-        df_rent.index.name = "RENT_SEQ"
+        # 데이터프레임 불러오기
+        df_user, df_book, df_rent = load_dataframes()
 
-        isbn = self.bookinfo.get_isbn()
-        seq = max(df_rent[df_rent["BOOK_ISBN"] == isbn].index)
-        rent_phone = df_rent["USER_PHONE"].loc[seq]
+        # 선택한 도서에 대한 ISBN과 도서명, 대여번호, 대여자 전화번호 불러오기
+        book_isbn = self.bookinfo.get_isbn()
+        book_title = df_book["BOOK_TITLE"].loc[book_isbn]
+        rent_seq = max(df_rent[df_rent["BOOK_ISBN"] == book_isbn].index)
+        rent_phone = df_rent["USER_PHONE"].loc[rent_seq]
+
+        # 이미 반납한 경우의 이벤트 처리
+        if df_rent["RENT_RETURN_DATE"].loc[rent_seq] != -1:
+            messagebox.showinfo("도서 반납", "이미 반납한 도서입니다.")
+            return 1
 
         # 반납일(오늘 날짜) 가져오기
         today = datetime.now()
         today_format = today.strftime("%Y%m%d")
 
-        df_rent["RENT_RETURN_DATE"].loc[seq] = today_format
+        # 반납 실행
+        df_rent["RENT_RETURN_DATE"].loc[rent_seq] = today_format
         df_user["USER_RENT_CNT"].loc[rent_phone] -= 1
 
         df_user.to_csv(DIR_CSV_USER, index=False, encoding='CP949')
         df_rent.to_csv(DIR_CSV_RENT, index=True, encoding='CP949')
 
-        messagebox.showinfo("도서 반납", "도서 반납 완료(이벤트 테스트)")
+        msg = "도서 반납 완료\n"
+        msg += f"- {book_title}({book_isbn})\n"
+        messagebox.showinfo("도서 반납", msg)
 
 # ===========================================================================================
